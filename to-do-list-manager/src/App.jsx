531756c63d9b1e145ad901b448111e25cac21248
sejal-27 <sejall2709@gmail.com>
2024-03-20 12:06:31 +0530

    installing postcss, autoprefixer and header file

--- to-do-list-manager/src/App.jsx
+++ to-do-list-manager/src/App.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <div >
-     hi
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
